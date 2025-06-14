54472f408747d1ae11e2910cb1e429e46d9a2b4b
Dito Ardi Pratama <dito010504@gmail.com>
2025-06-14 09:13:15 +0700

    update code

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,23 +1,23 @@
 function expandGame() {
-  document.getElementById('modal').style.display = 'block';
-  document.body.style.overflow = 'hidden';
+  document.getElementById("modal").style.display = "block";
+  document.body.style.overflow = "hidden";
 }
 
 function closeModal() {
-  document.getElementById('modal').style.display = 'none';
-  document.body.style.overflow = 'auto';
+  document.getElementById("modal").style.display = "none";
+  document.body.style.overflow = "auto";
 }
 
 function showGameDetail(gameName) {
-  const detailBox = document.getElementById('game-detail');
-  let content = '';
+  const detailBox = document.getElementById("game-detail");
+  let content = "";
 
-  if (gameName === 'music') {
+  if (gameName === "music") {
     content = `
       <h2>BLOCKLY MOVIE</h2>
       <p>Game edukasi yang mengenalkan konsep <strong>path-finding</strong> atau pencarian jalur menggunakan mobil balap. Cocok untuk mengenalkan logika dan algoritma sederhana.</p>
     `;
-  } else if (gameName === 'Meow Maze') {
+  } else if (gameName === "Meow Maze") {
     content = `
       <h2>BLOCKLY MUSICe</h2>
       <p>Bantu kucing imut melewati labirin sambil belajar <strong>algoritma dasar</strong> secara menyenangkan. Game ini ideal untuk siswa SD atau SMP.</p>
@@ -25,7 +25,7 @@ function showGameDetail(gameName) {
   }
 
   detailBox.innerHTML = content;
-  detailBox.style.display = 'block';
+  detailBox.style.display = "block";
 }
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -37,5 +37,3 @@ window.addEventListener("DOMContentLoaded", () => {
     if (bubbleImg) bubbleImg.src = "assets/images/bubble2.png";
   }, 3000); // 3 detik
 });
-
-
