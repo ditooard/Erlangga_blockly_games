594fb1d178dec681edbde75f011aa6c6e9b883bb
Dito Ardi Pratama <dito010504@gmail.com>
2025-06-14 11:49:17 +0700

    responsif

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,37 +3,31 @@ function expandGame() {
   document.body.style.overflow = "hidden";
 }
 
+  window.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('.game-card').forEach(card => {
+      card.classList.add('floating-card');
+    });
+  });
+
 function closeModal() {
   document.getElementById("modal").style.display = "none";
   document.body.style.overflow = "auto";
 }
 
-function showGameDetail(gameName) {
-  const detailBox = document.getElementById("game-detail");
-  let content = "";
-
-  if (gameName === "music") {
-    content = `
-      <h2>BLOCKLY MOVIE</h2>
-      <p>Game edukasi yang mengenalkan konsep <strong>path-finding</strong> atau pencarian jalur menggunakan mobil balap. Cocok untuk mengenalkan logika dan algoritma sederhana.</p>
-    `;
-  } else if (gameName === "Meow Maze") {
-    content = `
-      <h2>BLOCKLY MUSICe</h2>
-      <p>Bantu kucing imut melewati labirin sambil belajar <strong>algoritma dasar</strong> secara menyenangkan. Game ini ideal untuk siswa SD atau SMP.</p>
-    `;
-  }
-
-  detailBox.innerHTML = content;
-  detailBox.style.display = "block";
-}
 
 window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    const anakImg = document.getElementById("anak-img");
-    const bubbleImg = document.getElementById("bubble-img");
+  let toggle = true;
+  const anakImg = document.getElementById("anak-img");
+  const bubbleImg = document.getElementById("bubble-img");
 
-    if (anakImg) anakImg.src = "assets/images/dontol2.png";
-    if (bubbleImg) bubbleImg.src = "assets/images/bubble2.png";
-  }, 3000); // 3 detik
+  setInterval(() => {
+    if (anakImg) {
+      anakImg.src = toggle ? "assets/images/dontol2.png" : "assets/images/dontol.png";
+    }
+    if (bubbleImg) {
+      bubbleImg.src = toggle ? "assets/images/bubble2.png" : "assets/images/bubble.png";
+    }
+    toggle = !toggle;
+  }, 5000); // setiap 1 detik
 });
+
